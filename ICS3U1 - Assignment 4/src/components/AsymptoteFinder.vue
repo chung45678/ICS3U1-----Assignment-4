7f12e51b924a01c66f54cd42797e507000def2d1
Chung <xc.cx.chung@gmail.com>
2024-11-13 15:02:22 -0500

    created vue and vite environment

---
+++ ICS3U1 - Assignment 4/src/components/AsymptoteFinder.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+const aValue = ref(0);
+const bValue = ref(0);
+const cValue = ref(0);
+
+function pythagoreanTheorem() {
+  cValue.value = Math.sqrt(aValue.value * aValue.value + bValue.value * bValue.value);
+}
+</script>
+
+<template>
+  <form @submit.prevent="pythagoreanTheorem()">
+    <label>a Value:</label>
+    <input v-model="aValue" type="number" />
+    <label>b Value:</label>
+    <input v-model="bValue" type="number" />
+    <label>c Value (Result):</label>
+    <input v-model="cValue" type="number" readonly />
+    <input type="submit" value="Calculate" />
+  </form>
+</template>
+
+<style scoped>
+
+</style>
