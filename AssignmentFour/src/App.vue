87cb4b8ba2cefda473cf58d89b94a3f24773636f
Chung <xc.cx.chung@gmail.com>
2024-11-14 13:45:40 -0500

    set up cosinelaw vue

---
+++ AssignmentFour/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import CosineLaw from './components/CosineLaw.vue';
+</script>
+
+<template>
+  <div class="app-container">
+    <CosineLaw />
+
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+    display: grid;
+    grid-template-columns: 45vw 45vw;
+    gap: 100px;
+}
+</style>
---
+++ AssignmentFour/src/components/CosineLaw.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+
+const aSide = ref(0);
+const bSide = ref(0);
+const cAngle = ref(0);
+const cSide = ref(0);
+
+function cosineLaw() {
+    cSide.value = Math.sqrt(aSide ** 2 + bSide ** 2 - 2 * aSide * bSide * Math.cos(cAngle * (Math.PI / 180)));
+
+}
+</script>
+
+<template>
+  <form @submit.prevent="cosineLaw()">
+    <label>a Side Value:</label>
+    <input v-model="aSide" type="number" />
+    <label>b Side Value:</label>
+    <input v-model="bSide" type="number" />
+    <label>c Angle Value:</label>
+    <input v-model="cAngle" type="number" />
+    <label>c side (Result):</label>
+    <input v-model="cSide" type="number" readonly />
+    <input type="submit" value="Calculate" />
+  </form>
+</template>
+
+<style scoped>
+
+</style>
